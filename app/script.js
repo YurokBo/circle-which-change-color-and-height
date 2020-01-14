83ec2e936753d29ddcea29870c2f1fbe4731d2ee
YurokBo <vertel3012@mail.ru>
2020-01-14 22:19:18 +0300

    es6

--- app/script.js
+++ app/script.js
@@ -1,13 +1,18 @@
-document.addEventListener('input', e => {
-    const circle = document.querySelector('.circle'),
-        inputColor = document.querySelector('input[placeholder ="color"]'),
-        inputHeight = document.querySelector('input[placeholder ="height"]');
+const circle = document.querySelector('.circle');
 
+const changeColor = () => {
+    const inputColor = document.querySelector('input[placeholder ="color"]');
     circle.style.backgroundColor = inputColor.value;
-    circle.style.height = inputHeight.value + 'px';
+};
 
-    if (inputHeight.value === '') {
-        circle.style.height = 100 + 'px';
-    }
+const changeHeight = () => {
+    const inputHeight = document.querySelector('input[placeholder ="height"]');
+    inputHeight.value === '' ?
+        circle.style.height = 100 + 'px' :
+        circle.style.height = inputHeight.value + 'px';
+};
 
-})
+document.addEventListener('input', e => {
+    changeColor();
+    changeHeight()
+});
